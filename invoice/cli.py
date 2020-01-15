99c77d1f17c1b5c058525b784f48d54b6b7450c6
Helena Ohlsen <ohlsenhelena@gmail.com>
2020-01-15 11:20:20 +0100

    Add scraping component (#5)
    * Add types for cli options

--- invoice/cli.py
+++ invoice/cli.py
@@ -1,4 +1,5 @@
 import click
+import pandas as pd
 
 from invoice.load_data import read_invoice_file, read_server_info_file
 from invoice.process_invoice import populate_columns
@@ -6,12 +7,12 @@ from invoice.scraper import get_invoice_from_hetzner_account
 
 
 @click.command()
-@click.option("--user", help="Hetzner Account Username.")
-@click.option("--pw", help="Hetzner Account Password.")
-@click.option("--secret", help="Hetzner Account Base Secret.")
-@click.option("--filepath", help="Where invoice file will be saved.")
-def load_transform_invoice(user, pw, secret, filepath):
-    """Program to automatically download and transform Hetzner invoice data"""
+@click.option("--user", help="Hetzner Account Username.", type=str)
+@click.option("--pw", help="Hetzner Account Password.", type=str)
+@click.option("--secret", help="Hetzner Account Base Secret.", type=str)
+@click.option("--filepath", help="Where invoice file will be saved.", type=str)
+def load_transform_invoice(user, pw, secret, filepath) -> pd.DataFrame:
+    """Automatically download and transform most recent Hetzner invoice"""
     invoice_filepath = get_invoice_from_hetzner_account(user, pw, secret, filepath)
     invoice_data = read_invoice_file(invoice_filepath)
     server_data = read_server_info_file("hetzner-invoice/server_types.json")
